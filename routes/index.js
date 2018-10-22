be7ee374bfee4be1af714675632f15671c079cb8
thienlh96 <44220134+thienlh96@users.noreply.github.com>
2018-10-22 10:40:17 +0700

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -163,7 +163,7 @@ router.post('/facebook', function(req, res, next) {
 							});
 						});
 					} else if (messagingEvent.postback && messagingEvent.postback.payload == 'DKY'){
-						console.log('dky');
+						console.log('dkyhhhhh');
 						client.sendSenderAction(senderID, 'typing_on').then(() => {
 								client.sendTemplate(senderID,{
 								template_type: 'button',
